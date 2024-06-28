273ebdc3bf3ffd1c993383ae49e351a286f7da6f
Skuddin <sheikh@nextgenitltd.com>
2024-06-28 22:01:42 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="brand-logo" />
+        </div>
+
+        <ul>
+          <li>Menu</li>
+          <li>LOCATION</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default App
